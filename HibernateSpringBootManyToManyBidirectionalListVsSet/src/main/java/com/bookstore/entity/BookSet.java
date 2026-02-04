d48a52a10d9b7518a3dc0b52b0f953d5045115b4
anghelleonard <leoprivacy@yahoo.com>
2026-02-04 07:55:46 +0200

    Many-to-many use always set not list

---
+++ HibernateSpringBootManyToManyBidirectionalListVsSet/src/main/java/com/bookstore/entity/BookSet.java
@@ -0,0 +1,86 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class BookSet implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToMany(mappedBy = "books")
+    private Set<AuthorSet> authors = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<AuthorSet> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorSet> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object obj) {              
+        
+        if (this == obj) {
+            return true;
+        }
+        
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        return id != null && id.equals(((BookSet) obj).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2021;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", isbn=" + isbn + '}';
+    }    
+}
