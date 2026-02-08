70d5a1d2c73d4f15ff6c5dc52fb8814df90c4d1e
anghelleonard <leoprivacy@yahoo.com>
2026-02-08 10:57:49 +0200

    Many-to-many Set and Order By

---
+++ HibernateSpringBootManyToManySetAndOrderBy/src/main/resources/schema-mysql.sql
@@ -0,0 +1,31 @@
+-- Recreate database
+USE `bookstoredb`;
+DROP DATABASE IF EXISTS `bookstoredb`;
+CREATE DATABASE `bookstoredb`;
+USE `bookstoredb`;
+
+-- Table `author`
+CREATE TABLE `author` (
+  `id`    BIGINT        NOT NULL AUTO_INCREMENT,
+  `age`   INT(11)       NOT NULL,
+  `genre` VARCHAR(255)  DEFAULT NULL,
+  `name`  VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `author_pk` PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `book`
+CREATE TABLE `book` (
+  `id`        BIGINT        NOT NULL AUTO_INCREMENT,    
+  `title`     VARCHAR(255)  DEFAULT NULL,
+  `isbn`      VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `book_pk` PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `author_book`
+CREATE TABLE `author_book` (
+  `author_id` BIGINT        NOT NULL,
+  `book_id`   BIGINT        NOT NULL,
+  CONSTRAINT `author_book_uk` UNIQUE (`author_id`, `book_id`),
+  CONSTRAINT `author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT `book_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
