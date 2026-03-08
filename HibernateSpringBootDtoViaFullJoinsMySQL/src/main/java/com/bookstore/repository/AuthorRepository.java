2bab02d959f08ab5997cc6aadba4c2a51d5cc7fe
anghelleonard <leoprivacy@yahoo.com>
2026-03-08 09:30:59 +0200

    DTO Full Joins (MySQL)

---
+++ HibernateSpringBootDtoViaFullJoinsMySQL/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,31 @@
+package com.bookstore.repository;
+
+import java.util.List;
+import com.bookstore.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.bookstore.projection.AuthorNameBookTitle;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    // Fetch all authors and books (JPQL) 
+    @Query(value = "SELECT b.title AS title, a.name AS name "
+            + "FROM Author a LEFT JOIN a.books b UNION "
+            + "SELECT b.title AS title, a.name AS name "
+            + "FROM Author a RIGHT JOIN a.books b WHERE a.id IS NULL")
+    List<AuthorNameBookTitle> findAuthorsAndBooksJpql();
+
+    // Fetch all authors and books (SQL)    
+    @Query(value = "(SELECT b.title AS title, a.name AS name FROM author a "
+            + "LEFT JOIN book b ON a.id = b.author_id) "
+            + "UNION " //  will remove duplicates (use UNION ALL to keep duplicates)
+            + "(SELECT b.title AS title, a.name AS name FROM author a "
+            + "RIGHT JOIN book b ON a.id = b.author_id "
+            + "WHERE a.id IS NULL)",
+            nativeQuery = true)
+    List<AuthorNameBookTitle> findAuthorsAndBooksSql();
+}
