c3e232ea969b8d801ceb7df3f0b975b903cc567a
anghelleonard <leoprivacy@yahoo.com>
2026-04-13 11:02:05 +0300

    Batch inserts

---
+++ HibernateSpringBootBatchInsertsStructuredConcurrency/src/main/java/com/bookstore/impl/BatchExecutor.java
@@ -0,0 +1,72 @@
+package com.bookstore.impl;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope.Joiner;
+import static java.util.concurrent.StructuredTaskScope.open;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.StopWatch;
+
+@Component
+public class BatchExecutor<T> {
+
+    private static final Logger logger = Logger.getLogger(BatchExecutor.class.getName());
+
+    @Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
+    private int batchSize;
+
+    private final TransactionTemplate txTemplate;
+    private final EntityManager entityManager;
+
+    public BatchExecutor(TransactionTemplate txTemplate, EntityManager entityManager) {
+        this.txTemplate = txTemplate;
+        this.entityManager = entityManager;
+    }
+
+    public <S extends T> void saveInBatch(List<S> entities)
+            throws InterruptedException, ExecutionException {
+
+        txTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        StopWatch timer = new StopWatch();
+        timer.start();
+        executeBatch(entities);
+        timer.stop();
+
+        logger.info(() -> "\nBatch time: " + timer.getTotalTimeMillis()
+                + " ms (" + timer.getTotalTimeSeconds() + " s)");
+    }
+
+    public <S extends T> void executeBatch(List<S> list) throws InterruptedException {
+
+        int pos = 0;
+        try (var scope = open(Joiner.<Object>allSuccessfulOrThrow())) {
+
+            while ((pos + batchSize) < list.size()) {
+                int cpos = pos;                
+                scope.fork(() -> batch(list.subList(cpos, cpos + batchSize)));
+                pos = pos + batchSize;
+            }
+
+            // batch the remaining entities
+            int cpos = pos;            
+            scope.fork(() -> batch((List<S>) list.subList(cpos, list.size())));
+            
+            scope.join(); // Join subtasks, propagating exceptions
+        }
+    }
+
+    public <S extends T> void batch(List<S> list) {
+
+        txTemplate.executeWithoutResult(status -> {            
+            for (S entity : list) {
+                entityManager.persist(entity);
+            }
+        });
+    }
+}
