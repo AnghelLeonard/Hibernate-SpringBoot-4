4fa937d6e70d49f4385fd8c20db705d41651aa94
anghelleonard <leoprivacy@yahoo.com>
2026-04-24 07:34:24 +0300

    DTO and Element Collection

---
+++ HibernateSpringBootDtoElementCollection/src/main/java/com/bookstore/entity/ShoppingCart.java
@@ -0,0 +1,55 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ShoppingCart implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String owner;
+
+    // LAZY is default
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "shopping_cart_books",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"))
+    private List<Book> books = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
