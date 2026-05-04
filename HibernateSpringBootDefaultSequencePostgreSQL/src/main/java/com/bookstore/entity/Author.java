7ba9ff5f254c4ed7a3f7ee9d41d9d325b2395621
anghelleonard <leoprivacy@yahoo.com>
2026-05-04 07:51:33 +0300

    The HiLo algorithm

---
+++ HibernateSpringBootDefaultSequencePostgreSQL/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,35 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id     
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) // defaults to the "pooled" strategy
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
