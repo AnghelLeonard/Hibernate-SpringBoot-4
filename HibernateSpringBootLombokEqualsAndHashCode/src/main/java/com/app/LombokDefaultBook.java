3cd8df0345002a39bef0f7e88c6e215fcb27d6e0
anghelleonard <leoprivacy@yahoo.com>
2026-05-17 10:43:04 +0300

    Equals and hashCode

---
+++ HibernateSpringBootLombokEqualsAndHashCode/src/main/java/com/app/LombokDefaultBook.java
@@ -0,0 +1,46 @@
+package com.app;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@EqualsAndHashCode
+public class LombokDefaultBook implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ HibernateSpringBootLombokEqualsAndHashCode/src/test/java/com/app/test/IdManEqualsAndHashCodeTest.java
@@ -0,0 +1,115 @@
+package com.app.test;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.app.IdManBook;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.AssertionsKt.assertNotNull;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Rollback(false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class IdManEqualsAndHashCodeTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private static final IdManBook book = new IdManBook();
+    private static final Set<IdManBook> books = new HashSet<>();
+
+    @BeforeAll
+    public static void setUp() {
+        book.setId(1L);
+        book.setTitle("Modern History");
+        book.setIsbn("001-100-000-111");
+
+        books.add(book);
+    }
+
+    @Test
+    // Find in Set the book that has never been persisted
+    // State transition at assertion point: NEW
+    public void A_givenBookInSetWhenContainsThenTrue() throws Exception {
+
+        assertTrue(books.contains(book));
+    }
+
+    @Test
+    // Find in Set the book after it was persisted
+    // State transition at first assertion point: NEW
+    // State transition at second and third assertion point: MANAGED
+    public void B_givenBookWhenPersistThenSuccess() throws Exception {
+
+        assertNotNull(book.getId());
+
+        entityManager.persistAndFlush(book);
+        assertNotNull(book.getId());
+
+        assertTrue(books.contains(book));
+    }
+
+    @Test
+    // Find in Set the book after merge() was called - SELECT and UPDATE statement   
+    // State transition at assertion point: MANAGED
+    public void C_givenBookWhenMergeThenSuccess() throws Exception {
+
+        book.setTitle("New Modern History");
+        IdManBook mergedBook = entityManager.merge(book);
+        entityManager.flush();
+
+        assertTrue(books.contains(mergedBook));
+    }
+
+    @Test
+    // Find in Set the book after find() was called - SELECT statement    
+    // State transition at second assertion point: MANAGED
+    public void D_givenBookWhenFindThenSuccess() throws Exception {
+
+        IdManBook foundBook = entityManager.find(IdManBook.class, book.getId());
+        entityManager.flush();
+
+        assertTrue(books.contains(foundBook));
+    }
+
+    @Test
+    // Find in Set the book after detach() was called
+    // State transition at assertion point: DETACHED    
+    public void E_givenBookWhenFindAndDetachThenSuccess() throws Exception {
+
+        IdManBook foundBook = entityManager.find(IdManBook.class, book.getId());
+        entityManager.detach(foundBook);
+
+        assertTrue(books.contains(foundBook));
+    }
+
+    @Test
+    // Find in Set the book after remove() was called - DELETE statement
+    // State transition at assertion points: REMOVED    
+    public void F_givenBookWhenFindAndRemoveThenSuccess() throws Exception {
+
+        IdManBook foundBook = entityManager.find(IdManBook.class, book.getId());
+        entityManager.remove(foundBook);
+        entityManager.flush();
+
+        assertTrue(books.contains(foundBook));
+
+        books.remove(foundBook);
+
+        assertFalse(books.contains(foundBook));
+    }
+
+}
