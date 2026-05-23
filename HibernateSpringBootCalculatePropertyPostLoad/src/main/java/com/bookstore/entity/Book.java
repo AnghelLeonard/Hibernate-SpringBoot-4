ae07b5b20d7514b4fbb148ac93a4b4fc44245183
anghelleonard <leoprivacy@yahoo.com>
2026-05-23 08:09:54 +0300

    PostLoad

---
+++ HibernateSpringBootCalculatePropertyPostLoad/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,73 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.Transient;
+import java.io.Serializable;
+ 
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    private String title;
+    private String isbn;
+    private double price;
+
+    @Transient
+    private double discounted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscounted() {
+        return discounted;
+    }
+
+    @PostLoad
+    private void postLoad() {
+        this.discounted = this.price - this.price * 0.25; 
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", isbn=" 
+                + isbn + ", price=" + price + ", discounted=" + discounted + '}';
+    }        
+}
