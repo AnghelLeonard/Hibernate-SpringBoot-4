4212d572b677226f336071fe2af35e8dcea09b55
anghelleonard <leoprivacy@yahoo.com>
2026-05-24 07:33:56 +0300

    Hibernate Formula

---
+++ HibernateSpringBootCalculatePropertyFormula/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,69 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import java.io.Serializable;
+import org.hibernate.annotations.Formula;
+
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    private String title;
+    private String isbn;
+    private double price;
+
+    @Formula("price - price * 0.25")
+    private double discounted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Transient
+    public double getDiscounted() {
+        return discounted;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", isbn="
+                + isbn + ", price=" + price + ", discounted=" + discounted + '}';
+    }
+}
