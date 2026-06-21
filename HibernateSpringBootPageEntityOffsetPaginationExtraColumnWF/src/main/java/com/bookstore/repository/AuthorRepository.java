67e287e3f1004c518ecf2fe166687f8790fedd29
anghelleonard <leoprivacy@yahoo.com>
2026-06-21 17:24:09 +0300

    Entity pagination

---
+++ HibernateSpringBootPageEntityOffsetPaginationExtraColumnWF/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,17 @@
+package com.bookstore.repository;
+
+import com.bookstore.entity.Author;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+
+    @Transactional
+    @NativeQuery(value = "SELECT id, name, age, genre, COUNT(*) OVER() AS total FROM author")
+    List<Author> fetchAll(Pageable pageable);        
+}
