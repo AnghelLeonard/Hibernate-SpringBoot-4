fd203f3b8776c7057f53d84b3cf9cca835c5b657
anghelleonard <leoprivacy@yahoo.com>
2026-06-28 15:07:00 +0300

    Separate Listener Class via @EntityListeners

---
+++ HibernateSpringBootEntityListener/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,68 @@
+package com.bookstore.entity;
+
+import com.bookstore.listener.BookListener;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@EntityListeners(BookListener.class)
+public abstract class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", isbn=" + isbn + '}';
+    }
+
+}
---
+++ HibernateSpringBootEntityListener/src/main/java/com/bookstore/listener/BookListener.java
@@ -0,0 +1,48 @@
+package com.bookstore.listener;
+
+import com.bookstore.entity.Book;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostRemove;
+import jakarta.persistence.PostUpdate;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.PreUpdate;
+
+public class BookListener {
+
+    @PrePersist
+    void onPrePersist(Book book) {
+        System.out.println("BookListener.onPrePersist(): " + book);
+    }
+
+    @PostPersist
+    void onPostPersist(Book book) {
+        System.out.println("BookListener.onPostPersist(): " + book);
+    }
+
+    @PostLoad
+    void onPostLoad(Book book) {
+        System.out.println("BookListener.onPostLoad(): " + book);
+    }
+
+    @PreUpdate
+    void onPreUpdate(Book book) {
+        System.out.println("BookListener.onPreUpdate(): " + book);
+    }
+
+    @PostUpdate
+    void onPostUpdate(Book book) {
+        System.out.println("BookListener.onPostUpdate(): " + book);
+    }
+
+    @PreRemove
+    void onPreRemove(Book book) {
+        System.out.println("BookListener.onPreRemove(): " + book);
+    }
+
+    @PostRemove
+    void onPostRemove(Book book) {
+        System.out.println("BookListener.onPostRemove(): " + book);
+    }
+}
