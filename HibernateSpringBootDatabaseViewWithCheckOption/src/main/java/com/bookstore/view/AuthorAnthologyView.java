c8df974b62afde7ac1ccc4aa0e9350f11e26c449
anghelleonard <leoprivacy@yahoo.com>
2026-07-08 08:08:17 +0300

    WITH CHECK OPTION

---
+++ HibernateSpringBootDatabaseViewWithCheckOption/src/main/java/com/bookstore/view/AuthorAnthologyView.java
@@ -0,0 +1,61 @@
+package com.bookstore.view;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "author_anthology_view")
+public class AuthorAnthologyView implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorAnthologyView{" + "id=" + id + ", name=" + name 
+                + ", age=" + age + ", genre=" + genre + '}';
+    }    
+}
---
+++ HibernateSpringBootDatabaseViewWithCheckOption/src/main/resources/data-mysql.sql
@@ -0,0 +1,21 @@
+-- insert authors
+INSERT INTO `author` (`age`, `name`, `genre`, `royalties`, `sellrank`, `rating`, `id`) VALUES 
+  (23, "Mark Janel", "Anthology", 1200, 289, 3, 1),
+  (43, "Olivia Goy", "Horror", 4000, 490, 5, 2),
+  (51, "Quartis Young", "Anthology", 900, 122, 4, 3),
+  (34, "Joana Nimar", "History", 5600, 554, 4, 4),
+  (47, "Kakki Jou", "Anthology", 1000, 231, 5, 5),
+  (56, "Fair Pouille", "Anthology", 3400, 344, 5, 6);
+
+CREATE OR REPLACE VIEW AUTHOR_ANTHOLOGY_VIEW
+AS
+SELECT
+    a.id,
+    a.name,
+    a.age,
+    a.genre
+FROM
+    author a
+WHERE a.genre = "Anthology" WITH CHECK OPTION;
+
+-- use: ...  = "Anthology" WITH CHECK OPTION; for ensuring that the this operation is conformed with the definition of the view
