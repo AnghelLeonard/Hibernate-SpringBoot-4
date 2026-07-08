2d7e08eb6d4db83d8b3dd77c7cc21062d80d2710
anghelleonard <leoprivacy@yahoo.com>
2026-07-08 07:51:39 +0300

    Updatable database views

---
+++ HibernateSpringBootDatabaseViewUpdateInsertDelete/src/main/resources/schema-mysql.sql
@@ -0,0 +1,16 @@
+-- Recreate database
+DROP DATABASE IF EXISTS `bookstoredb`;
+CREATE DATABASE `bookstoredb`;
+USE `bookstoredb`;
+
+-- Table `author`
+CREATE TABLE `author` (
+  `id`        BIGINT        NOT NULL AUTO_INCREMENT,
+  `age`       INT(11)       NOT NULL,
+  `genre`     VARCHAR(255)  DEFAULT NULL,
+  `name`      VARCHAR(255)  DEFAULT NULL,
+  `sellrank`  INT(11)       DEFAULT NULL,
+  `royalties` INT(11)       DEFAULT NULL,
+  `rating`    INT(11)       DEFAULT NULL,
+  CONSTRAINT `author_pk` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
