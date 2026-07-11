99a0b66ee5e3900c3e6e563ed4556aa3761e870b
anghelleonard <leoprivacy@yahoo.com>
2026-07-11 08:20:26 +0300

    IN padding

---
+++ HibernateSpringBootINListPadding/src/main/resources/data-sqlserver.sql
@@ -0,0 +1,12 @@
+-- insert authors
+INSERT INTO author (age, name, genre) VALUES 
+  (23, 'Mark Janel', 'Anthology'),
+  (43, 'Olivia Goy', 'Horror'),
+  (51, 'Quartis Young', 'Anthology'),
+  (34, 'Joana Nimar', 'History'),
+  (38, 'Alicia Tom', 'Anthology'),
+  (54, 'Katy Loin', 'Anthology'),
+  (33, 'Marting Lyer', 'Anthology'),
+  (54, 'Sonya Ali', 'History'),
+  (53, 'Mark Junou', 'Anthology'),
+  (48, 'Sandra Toy', 'Horror');
