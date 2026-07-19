be526892afa6ac1f55cf05d20cb800867fa96c7e
anghelleonard <leoprivacy@yahoo.com>
2026-07-19 11:04:41 +0300

    Property Expression

---
+++ HibernateSpringBootPropertyExpressions/src/main/resources/schema-mysql.sql
@@ -0,0 +1,32 @@
+-- Recreate database
+DROP DATABASE IF EXISTS `bookstoredb`;
+CREATE DATABASE `bookstoredb`;
+USE `bookstoredb`;
+
+-- Table `author`
+CREATE TABLE `author` (
+  `id`    BIGINT        NOT NULL AUTO_INCREMENT,
+  `age`   INT(11)       NOT NULL,
+  `genre` VARCHAR(255)  DEFAULT NULL,
+  `name`  VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `author_pk` PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `book`
+CREATE TABLE `book` (
+  `id`        BIGINT        NOT NULL AUTO_INCREMENT,  
+  `author_id` BIGINT        NOT NULL,
+  `title`     VARCHAR(255)  DEFAULT NULL,
+  `isbn`      VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `book_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `book_author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `review`
+CREATE TABLE `review` (
+  `id`        BIGINT        NOT NULL AUTO_INCREMENT,  
+  `book_id`   BIGINT        NOT NULL,
+  `content`   VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `review_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `book_review_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
