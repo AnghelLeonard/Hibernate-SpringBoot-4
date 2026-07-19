ccfb39d2fa6173492465400428d0f20dd371370d
anghelleonard <leoprivacy@yahoo.com>
2026-07-19 16:20:26 +0300

    PostgreSQL SKIP LOCKED

---
+++ HibernateSpringBootPostgresSqlSkipLocked/src/main/resources/data-postgresql.sql
@@ -0,0 +1,9 @@
+-- insert books
+INSERT INTO book (isbn, title, status, id) VALUES 
+  ('001-JN', 'A History of Ancient Prague', 'PENDING', 1),
+  ('002-JN', 'A People''s History', 'PENDING', 2),
+  ('001-MJ', 'The Beatles Anthology', 'PENDING', 3),
+  ('001-OG', 'Carrie', 'PENDING', 4),
+  ('002-OG', 'Fragments of Horror', 'PENDING', 5),
+  ('002-MJ', 'Anthology Mission', 'PENDING', 6),
+  ('003-OG', 'Summer Of Night', 'PENDING', 7);
