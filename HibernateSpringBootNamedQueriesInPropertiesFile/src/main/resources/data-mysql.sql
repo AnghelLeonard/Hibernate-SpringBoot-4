03d85cb8eb52527d42276de4fb7597186710a00c
anghelleonard <leoprivacy@yahoo.com>
2026-07-19 09:26:04 +0300

    Named queries in properties file

---
+++ HibernateSpringBootNamedQueriesInPropertiesFile/src/main/resources/data-mysql.sql
@@ -0,0 +1,10 @@
+-- insert authors
+INSERT INTO `author` (`age`, `name`, `genre`, `id`) VALUES 
+  (23, "Mark Janel", "Anthology", 1),
+  (43, "Olivia Goy", "Horror", 2),
+  (51, "Quartis Young", "Anthology", 3),
+  (34, "Joana Nimar", "History", 4),
+  (38, "Alicia Tom", "Anthology", 5),
+  (56, "Katy Loin", "Anthology", 6),
+  (53, "Doil Kia", "Anthology", 7)
+ON DUPLICATE KEY UPDATE `id`=`id`;
