121dd91b3648b4044dc1fe207d59303851e2190c
anghelleonard <leoprivacy@yahoo.com>
2026-07-20 12:10:20 +0300

    Optimistic Force Increment

---
+++ HibernateSpringBootOptimisticForceIncrement/src/main/java/com/bookstore/entity/Modification.java
@@ -0,0 +1,57 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Entity
+public class Modification implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String description;
+    private String modification;
+    
+    @ManyToOne(fetch = FetchType.LAZY)  
+    private Chapter chapter;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+        
+    public String getModification() {
+        return modification;
+    }
+
+    public void setModification(String modification) {
+        this.modification = modification;
+    }
+
+    public Chapter getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(Chapter chapter) {
+        this.chapter = chapter;
+    }   
+}
---
+++ HibernateSpringBootOptimisticForceIncrement/src/main/resources/data-mysql.sql
@@ -0,0 +1,3 @@
+-- insert chapters
+INSERT INTO `chapter` (`title`, `content`, `version`, `id`) VALUES 
+  ("Locking", "Chapter content ...", 0, 1);
