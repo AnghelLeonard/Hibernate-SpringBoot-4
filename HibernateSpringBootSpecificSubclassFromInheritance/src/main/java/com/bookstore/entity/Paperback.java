d4c549c6648d9381ad6a36c3e132d485de979b4f
anghelleonard <leoprivacy@yahoo.com>
2026-08-02 09:26:52 +0300

    Single table inheritance

---
+++ HibernateSpringBootSpecificSubclassFromInheritance/src/main/java/com/bookstore/entity/Paperback.java
@@ -0,0 +1,38 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import java.io.Serializable;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Paperback extends Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private String sizeIn;
+    @NotNull
+    private String weightLbs;
+
+    public String getSizeIn() {
+        return sizeIn;
+    }
+
+    public void setSizeIn(String sizeIn) {
+        this.sizeIn = sizeIn;
+    }
+
+    public String getWeightLbs() {
+        return weightLbs;
+    }
+
+    public void setWeightLbs(String weightLbs) {
+        this.weightLbs = weightLbs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Paperback{" + "sizeIn=" + sizeIn + ", weightLbs=" + weightLbs + '}';
+    }
+
+}
---
+++ HibernateSpringBootSpecificSubclassFromInheritance/src/main/resources/schema-mysql.sql
@@ -0,0 +1,27 @@
+-- Recreate database
+DROP DATABASE IF EXISTS `bookstoredb`;
+CREATE DATABASE `bookstoredb`;
+USE `bookstoredb`;
+
+-- Table `author`
+CREATE TABLE `author` (
+  `id`    BIGINT        NOT NULL AUTO_INCREMENT,
+  `age`   INT(11)       NOT NULL,
+  `genre` VARCHAR(255)  DEFAULT NULL,
+  `name`  VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `author_pk` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `book`
+CREATE TABLE `book` (
+  `id`        BIGINT         NOT NULL AUTO_INCREMENT,  
+  `author_id` BIGINT,
+  `dtype`     VARCHAR(31)    NOT NULL CHECK ((`dtype` IN ('Book','Ebook','Paperback'))),
+  `format`    VARCHAR(255),
+  `isbn`      VARCHAR(255),
+  `size_in`   VARCHAR(255), 
+  `title`     VARCHAR(255),
+  `weight_lbs` VARCHAR(255),
+  CONSTRAINT `book_pk` PRIMARY KEY (id),
+  CONSTRAINT `book_author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
