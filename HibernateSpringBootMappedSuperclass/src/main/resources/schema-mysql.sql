8e0b587b4225d9e91506e92cf045507ada9c4c80
anghelleonard <leoprivacy@yahoo.com>
2026-08-02 16:39:49 +0300

    MappedSuperclass

---
+++ HibernateSpringBootMappedSuperclass/src/main/resources/schema-mysql.sql
@@ -0,0 +1,38 @@
+-- Recreate database
+USE `bookstoredb`;
+DROP DATABASE IF EXISTS `bookstoredb`;
+CREATE DATABASE `bookstoredb`;
+USE `bookstoredb`;
+
+-- Table `author`
+CREATE TABLE `author` (
+  `id`    BIGINT        NOT NULL AUTO_INCREMENT,
+  `age`   INT(11)       NOT NULL,
+  `genre` VARCHAR(255)  DEFAULT NULL,
+  `name`  VARCHAR(255)  DEFAULT NULL,
+  CONSTRAINT `author_pk` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `ebook`
+CREATE TABLE `ebook` (  
+  `id`        BIGINT        NOT NULL AUTO_INCREMENT,
+  `author_id` BIGINT        NOT NULL,
+  `format`    VARCHAR(255)  DEFAULT NULL,  
+  `isbn`      VARCHAR(255)  DEFAULT NULL,  
+  `title`     VARCHAR(255)  DEFAULT NULL,  
+  CONSTRAINT `author_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `ebook_author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table `paperback`
+CREATE TABLE `paperback` (  
+  `id`          BIGINT        NOT NULL AUTO_INCREMENT,
+  `author_id`   BIGINT        NOT NULL,
+  `isbn`        VARCHAR(255)  DEFAULT NULL,  
+  `size_in`     VARCHAR(255)  DEFAULT NULL,  
+  `title`       VARCHAR(255)  DEFAULT NULL,  
+  `weight_lbs`  VARCHAR(255)  DEFAULT NULL,  
+  CONSTRAINT `paperback_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `paperback_author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
