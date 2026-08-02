af0c0fda8e677f1d892a50ee9f8d23ae23383afd
anghelleonard <leoprivacy@yahoo.com>
2026-08-02 13:20:11 +0300

    Join Table Inheritance and Strategy pattern

---
+++ HibernateSpringBootJoinedAndStrategy/src/main/java/com/bookstore/entity/Paperback.java
@@ -0,0 +1,37 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import java.io.Serializable;
+
+@Entity
+@PrimaryKeyJoinColumn(name="paperback_book_id")
+public class Paperback extends Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String sizeIn;
+    private String weightLbs;
+
+    public String getSizeIn() {
+        return sizeIn;
+    }
+
+    public void setSizeIn(String sizeIn) {
+        this.sizeIn = sizeIn;
+    }
+
+    public String getWeightLbs() {
+        return weightLbs;
+    }
+
+    public void setWeightLbs(String weightLbs) {
+        this.weightLbs = weightLbs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Paperback{" + "sizeIn=" + sizeIn + ", weightLbs=" + weightLbs + '}';
+    }
+
+}
