4d3c46178ee5c6afd7959989e9a5570567bb0876
anghelleonard <leoprivacy@yahoo.com>
2026-08-06 14:09:50 +0300

    Best many-to-many mapping

--- HibernateSpringBootManyToManyBidirectional/src/main/resources/schema-mysql.sql
+++ HibernateSpringBootManyToManyBidirectional/src/main/resources/schema-mysql.sql
@@ -1,5 +1,4 @@
 -- Recreate database
-USE `bookstoredb`;
 DROP DATABASE IF EXISTS `bookstoredb`;
 CREATE DATABASE `bookstoredb`;
 USE `bookstoredb`;
@@ -10,7 +9,7 @@ CREATE TABLE `author` (
   `age`   INT(11)       NOT NULL,
   `genre` VARCHAR(255)  DEFAULT NULL,
   `name`  VARCHAR(255)  DEFAULT NULL,
-  CONSTRAINT `author_pk` PRIMARY KEY (id)
+  CONSTRAINT `author_pk` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Table `book`
@@ -18,13 +17,13 @@ CREATE TABLE `book` (
   `id`        BIGINT        NOT NULL AUTO_INCREMENT,    
   `title`     VARCHAR(255)  DEFAULT NULL,
   `isbn`      VARCHAR(255)  DEFAULT NULL,
-  CONSTRAINT `book_pk` PRIMARY KEY (id)
+  CONSTRAINT `book_pk` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Table `author_book`
 CREATE TABLE `author_book` (
-  `author_id` BIGINT        NOT NULL,
-  `book_id`   BIGINT        NOT NULL,
+  `author_id` BIGINT  NOT NULL,
+  `book_id`   BIGINT  NOT NULL,
   CONSTRAINT `author_book_uk` UNIQUE (`author_id`, `book_id`),
   CONSTRAINT `author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT `book_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION  
