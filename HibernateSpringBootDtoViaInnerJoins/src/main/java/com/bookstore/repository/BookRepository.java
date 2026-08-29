8596bf6861e649a594e32c556352d6544354c2a5
anghelleonard <leoprivacy@yahoo.com>
2026-08-29 08:51:05 +0300

    Writing Joins

--- HibernateSpringBootDtoViaInnerJoins/src/main/java/com/bookstore/repository/BookRepository.java
+++ HibernateSpringBootDtoViaInnerJoins/src/main/java/com/bookstore/repository/BookRepository.java
@@ -7,30 +7,39 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.bookstore.projection.AuthorNameBookTitle;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 @Repository
 @Transactional(readOnly = true)
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     // Fetch books and authors (JPQL)
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM Book b INNER JOIN b.author a")
+    @Query(value = """
+                   SELECT b.title AS title, a.name AS name
+                   FROM Book b INNER JOIN b.author a
+                   """)
     List<AuthorNameBookTitle> findBooksAndAuthorsJpql();
 
     // Fetch books and authors (SQL)
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM book b INNER JOIN author a ON a.id = b.author_id",
-            nativeQuery = true)
+    @NativeQuery(value = """
+                   SELECT b.title AS title, a.name AS name
+                   FROM book b INNER JOIN author a ON a.id = b.author_id
+                   """)
     List<AuthorNameBookTitle> findBooksAndAuthorsSql();
     
     // Fetch books and authors filtering by author's genre and book's price (JPQL)
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM Book b INNER JOIN b.author a WHERE a.genre = ?1 AND b.price < ?2")
+    @Query(value = """
+                   SELECT b.title AS title, a.name AS name
+                   FROM Book b INNER JOIN b.author a 
+                   WHERE a.genre = ?1 AND b.price < ?2
+                   """)
     List<AuthorNameBookTitle> findBooksAndAuthorsByGenreAndPriceJpql(String genre, int price);
 
     // Fetch books and authors filtering by author's genre and book's price (SQL)
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM book b INNER JOIN author a ON a.id = b.author_id WHERE a.genre = ?1 AND b.price < ?2",
-            nativeQuery = true)
+    @NativeQuery(value = """
+                   SELECT b.title AS title, a.name AS name
+                   FROM book b INNER JOIN author a ON a.id = b.author_id 
+                   WHERE a.genre = ?1 AND b.price < ?2
+                   """)
     List<AuthorNameBookTitle> findBooksAndAuthorsByGenreAndPriceSql(String genre, int price);
 }
