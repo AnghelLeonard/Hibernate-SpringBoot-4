8ffc4dc56444918a0db9381d812d6132fb290bde
anghelleonard <leoprivacy@yahoo.com>
2026-08-29 09:20:48 +0300

    Writing Joins

--- HibernateSpringBootDtoViaRightExcludingJoins/src/main/java/com/bookstore/repository/BookRepository.java
+++ HibernateSpringBootDtoViaRightExcludingJoins/src/main/java/com/bookstore/repository/BookRepository.java
@@ -7,19 +7,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.bookstore.projection.AuthorNameBookTitle;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 @Repository
 @Transactional(readOnly = true)
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     // Fetch books and authors excluding books that have registered authors (JPQL)    
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM Book b RIGHT JOIN b.author a WHERE b.id IS NULL")
+    @Query(value = """
+                   SELECT b.title AS title, a.name AS name
+                   FROM Book b RIGHT JOIN b.author a WHERE b.id IS NULL
+                   """)
     List<AuthorNameBookTitle> findBooksAndAuthorsJpql();
 
     // Fetch books and authors excluding books that have registered authors (SQL)    
-    @Query(value = "SELECT b.title AS title, a.name AS name "
-            + "FROM book b RIGHT JOIN author a ON a.id = b.author_id WHERE b.id IS NULL",
-            nativeQuery = true)
+    @NativeQuery(value = """
+                         SELECT b.title AS title, a.name AS name
+                         FROM book b RIGHT JOIN author a ON a.id = b.author_id 
+                         WHERE b.id IS NULL
+                         """)
     List<AuthorNameBookTitle> findBooksAndAuthorsSql();        
 }
