289c87eaa98f23e1ed0f07ec3201b5770afd5337
anghelleonard <leoprivacy@yahoo.com>
2026-08-29 09:35:04 +0300

    Writing Joins

--- HibernateSpringBootDtoViaCrossJoins/src/main/java/com/bookstore/repository/FormatRepository.java
+++ HibernateSpringBootDtoViaCrossJoins/src/main/java/com/bookstore/repository/FormatRepository.java
@@ -7,19 +7,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.bookstore.projection.BookTitleAndFormatType;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 @Repository
 @Transactional(readOnly = true)
 public interface FormatRepository extends JpaRepository<Format, Long> {
 
     // Cross join formats and books (JPQL)    
-    @Query(value = "SELECT b.title AS title, f.formatType AS formatType "
-            + "FROM Format f, Book b")
+    @Query(value = """
+                   SELECT b.title AS title, f.formatType AS formatType
+                   FROM Format f, Book b
+                   """)
     List<BookTitleAndFormatType> findFormatsAndBooksJpql();
 
     // Cross join formats and books (SQL)    
-    @Query(value = "SELECT b.title AS title, f.format_type AS formatType "
-            + "FROM format f CROSS JOIN book b",
-            nativeQuery = true)
+    @NativeQuery(value = """
+                         SELECT b.title AS title, f.format_type AS formatType
+                         FROM format f CROSS JOIN book b
+                         """)
     List<BookTitleAndFormatType> findFormatsAndBooksSql();
 }
