c99f236cca0148a88e7ebe8746e527bb37d98219
anghelleonard <leoprivacy@yahoo.com>
2026-08-30 09:52:23 +0300

    delete

--- HibernateSpringBootProjectionAndCollectionsBenchmark/src/main/java/com/bookstore/spring/dto/SimpleAuthorDto.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore.spring.dto;
-
-public interface SimpleAuthorDto {
-
-    public String getName();  // of author
-    public String getGenre(); // of author
-    public String getTitle(); // of book
-}
