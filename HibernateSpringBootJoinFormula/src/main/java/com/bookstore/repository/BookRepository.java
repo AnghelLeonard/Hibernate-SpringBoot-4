35ab40756a6653162990a0d7fa187be63c03682f
anghelleonard <leoprivacy@yahoo.com>
2026-09-24 07:31:53 +0300

    Hibernate JoinFormula

--- HibernateSpringBootJoinFormula/src/main/java/com/bookstore/repository/BookRepository.java
+++ HibernateSpringBootJoinFormula/src/main/java/com/bookstore/repository/BookRepository.java
@@ -11,9 +11,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Transactional(readOnly = true)
     @Query(value = """
-                   SELECT * FROM book WHERE price < ?1 AND author_id = ?2
-                   ORDER BY price DESC LIMIT 1
-                   """,
-            nativeQuery = true)
+                   SELECT b FROM Book b WHERE b.price < ?1 AND b.author.id = ?2
+                   ORDER BY b.price DESC LIMIT 1
+                   """)
     Book fetchNextSmallerPrice(int price, long authorId);
 }
